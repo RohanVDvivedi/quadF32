d41dc2b38e4c93590e7e2a20d0d9c4a42ef3782f
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-03-21 17:10:01 +0530

    uart built but no success

--- main.c
+++ main.c
@@ -10,9 +10,12 @@
 #define RCC_APB2ENR (*((volatile uint32_t*)(RCC_BASE + 0x18)))
 
 #define GPIOC_BASE 0x40011000
-
 #define GPIOC_CRH (*((volatile uint32_t*)(GPIOC_BASE + 0x04)))
 #define GPIOC_ODR (*((volatile uint32_t*)(GPIOC_BASE + 0x0c)))
+
+#define GPIOA_BASE 0x40010800
+#define GPIOA_CRH (*((volatile uint32_t*)(GPIOC_BASE + 0x04)))
+#define GPIOA_ODR (*((volatile uint32_t*)(GPIOC_BASE + 0x0c)))
 
 void delay_for(volatile int clocks)
 {
@@ -24,28 +27,30 @@ void delay_for(volatile int clocks)
 
 void main(void)
 {
-	RCC_APB2ENR |= ((1<<14) | (1<<4) | (1<<0));
+	RCC_APB2ENR |= ((1<<14) | (1<<4) | (1<<2) | (1<<0));
+
 	GPIOC_CRH   &= 0xFF0FFFFF;
 	GPIOC_CRH   |= 0x00200000;
 
-		GPIOC_ODR |= (1 << 13);
-		delay_for(1000000);
-		GPIOC_ODR &= (~(1 << 13));
-		delay_for(1000000);
+	GPIOA_CRH   &= 0xFFFFF00F;
+	GPIOA_CRH   |= 0x000004d0;
+
 	uart_init(57600);
-		GPIOC_ODR |= (1 << 13);
-		delay_for(1000000);
-		GPIOC_ODR &= (~(1 << 13));
-		delay_for(1000000);
-	while(1)
+
+	char c = 'X';
+
+	while(c = uart_read_byte())
 	{
 		GPIOC_ODR |= (1 << 13);
 		delay_for(1000000);
 		GPIOC_ODR &= (~(1 << 13));
 		delay_for(1000000);
-		
+
 		uart_write_byte('H');
 		uart_write_byte('i');
+		uart_write_byte(' ');
+		uart_write_byte(c);
+		uart_write_byte('\r');
 		uart_write_byte('\n');
 	}
 }
--- uart/uart.c
+++ uart/uart.c
@@ -15,32 +15,31 @@ int uart_init(uint32_t baud_rate)
 	volatile uint32_t* USART_BRRx = USART + USART_BRR;
 	volatile uint32_t* USART_CR1x = USART + USART_CR1;
 
-	*USART_CR1x |= (1<<13);
-	*USART_CR1x &= (~(1<<12));
+	//*USART_CR1x &= (~(1<<12));
 
 	double USART_DIV = 72000000.0/(16.0 * baud_rate);
 	uint32_t BRR_val = (((uint32_t)USART_DIV) << 4) + ((uint32_t)((USART_DIV-((double)((uint32_t)USART_DIV))) * 16));
 
-	*USART_BRRx = BRR_val;
+	*USART_BRRx = 0x341;
 
-	*USART_CR1x |= (1<<3);
-	*USART_CR1x |= (1<<2);
+	*USART_CR1x |= ((1<<13) | (1<<3) | (1<<2));
 }
 
 void uart_write_byte(char byte)
 {
 	volatile uint32_t* USART_SRx = USART + USART_SR;
 	volatile uint32_t* USART_DRx = USART + USART_DR;
+	(*USART_DRx) = byte;
 	while(((*USART_SRx) & (1<<7))==0){}
-	*USART_DRx = byte;
 }
 
 char uart_read_byte()
 {
 	volatile uint32_t* USART_SRx = USART + USART_SR;
 	volatile uint32_t* USART_DRx = USART + USART_DR;
-	while(((*USART_SRx) & (1<<5))==0){}
-	return (*USART_DRx);
+	int i = 0;
+	while(((*USART_SRx) & (1<<5))==0 && i < 1000000){i++;}
+	return i==1000000 ? 'X' : ((*USART_DRx) & 0xff);
 }
 
 int uart_write(char* str, unsigned int bytes_to_write)

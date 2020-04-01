f86a7f740749a7c899dbfbe6e70e0858c24015f7
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-01 11:44:57 +0530

    since all the registers for all the timers are idnetically structures, we club all of them as just timers

--- regs/adv_tim.h
+++ regs/adv_tim.h
@@ -1,10 +1,10 @@
-#ifndef ADVANCED_TIMER_REGS_H
-#define ADVANCED_TIMER_REGS_H
+#ifndef TIMER_REGS_H
+#define TIMER_REGS_H
 
 #include<stdint.h>
 
-typedef struct ADV_TIM_t ADV_TIM_t;
-struct ADV_TIM_t
+typedef struct TIMER_t TIMER_t;
+struct TIMER_t
 {
 	uint32_t TIM_CR1;
 	uint32_t TIM_CR2;
@@ -28,7 +28,19 @@ struct ADV_TIM_t
 	uint32_t TIM_DMAR;
 };
 
-#define TIM1 ((volatile ADV_TIM_t*)(0x40012C00))
-#define TIM8 ((volatile ADV_TIM_t*)(0x40013400))
+#define TIM1 ((volatile TIMER_t*)(0x40012C00))
+#define TIM2 ((volatile TIMER_t*)(0x40000000))
+#define TIM3 ((volatile TIMER_t*)(0x40000400))
+#define TIM4 ((volatile TIMER_t*)(0x40000800))
+#define TIM5 ((volatile TIMER_t*)(0x40000C00))
+#define TIM6 ((volatile TIMER_t*)(0x40001000))
+#define TIM7 ((volatile TIMER_t*)(0x40001400))
+#define TIM8 ((volatile TIMER_t*)(0x40013400))
+#define TIM9 ((volatile TIMER_t*)(0x40014C00))
+#define TIM10 ((volatile TIMER_t*)(0x40015000))
+#define TIM11 ((volatile TIMER_t*)(0x40015400))
+#define TIM12 ((volatile TIMER_t*)(0x40001800))
+#define TIM13 ((volatile TIMER_t*)(0x40001C00))
+#define TIM14 ((volatile TIMER_t*)(0x40002000))
 
 #endif

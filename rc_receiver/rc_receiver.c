f14ff860ea33a2b4cae500bb27de6b981a269a7d
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-04 19:28:51 +0530

    using traditional style interrupt based any pin rc receiver captre, istead t of my new idea of relying solely on timer input captures

--- rc_receiver/rc_receiver.c
+++ rc_receiver/rc_receiver.c
@@ -1,17 +1,49 @@
 #include<rc_receiver/rc_receiver.h>
 
+// channel_start with -1 means it is uninitialized or had an error in last read
+static volatile int32_t channel_start[6] = {-1, -1, -1, -1, -1, -1};
+
+static volatile uint32_t channel_value[6];
+
 void init_rc_receiver()
 {
-	// using pins PA6,7 and PB0,1 with Timer 3
-	RCC->RCC_APB1ENR |= (1<<1);
-	RCC->RCC_APB2ENR |= ((1<<3) | (1<<2));
+	// turn on timer 5 clock and the PB10-PB15 pins ports clocks
+	RCC_APB1ENR |= (1<<3);
+	RCC_APB2ENR |= (1<<3);
 
-	GPIOA->GPIO_CRL = (GPIOA->GPIO_CRL & ~(0xff<<24)) | 0xbb;
-	GPIOB->GPIO_CRL = (GPIOB->GPIO_CRL & ~(0xff)) | 0xbb;
+	// setup timer 5 ticking every microsecond
+	TIM2->TIM_CNT = 0;
 
-	//TODO :: setup timer counters appropriately, as input capture
+	TIM5->TIM_CR1 |= (1<<0);
 
-	TIM3->TIM_CR1 |= (1<<0);
+	// setup six interrupts on the port
+}
+
+void edge_interrupt_rc_channel(void)
+{
+	uint32_t timer_counter_value = TIM5->TIM_CNT;
+
+	int channel_no;
+	for(channel_no = 0; channel_no <= 15; channel_no++)
+	{
+		int channel_pin = channel_no + 10;
+		// if the interrupt came from this pin
+		{
+			if(GPIOB->INPUT_REGISTER | (1<<channel_pin))
+			{
+				channel_start[channel_no] = timer_counter_value
+			}
+			else
+			{
+				if(timer_counter_value < channel_start)
+				{
+					timer_counter_value += 65536
+				}
+				channel_value[channel_no] = timer_counter_value - channel_start[channel_no];
+				channel_start[channel_no] = -1;
+			}
+		}
+	}
 }
 
 static uint32_t compare_and_map_and_range(uint32_t value)
@@ -28,10 +60,11 @@ static uint32_t compare_and_map_and_range(uint32_t value)
 	return value;
 }
 
-void get_rc_channels(uint32_t* c1, uint32_t* c2, uint32_t* c3, uint32_t* c4)
+void get_rc_channels(uint32_t chan_ret[6])
 {
-	(*c1) = compare_and_map_and_range(TIM2->TIM_CCR1);
-	(*c2) = compare_and_map_and_range(TIM2->TIM_CCR2);
-	(*c3) = compare_and_map_and_range(TIM2->TIM_CCR3);
-	(*c4) = compare_and_map_and_range(TIM2->TIM_CCR4);
+	int iter_ch;
+	for(iter_ch = 0; iter_ch < 6; iter_ch++)
+	{
+		chan_ret[iter_ch] = compare_and_map_and_range(channel_value[iter_ch]);
+	}
 }
--- rc_receiver/rc_receiver.h
+++ rc_receiver/rc_receiver.h
@@ -7,6 +7,6 @@
 
 void init_rc_receiver();
 
-void get_rc_channels(uint32_t* c1, uint32_t* c2, uint32_t* c3, uint32_t* c4);
+void get_rc_channels(uint32_t chan_ret[6]);
 
 #endif

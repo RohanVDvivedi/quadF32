77a3c46f6d2ed80bde2542352bd7e9d89c1da668
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-17 22:04:12 +0530

    trying to read data over uart mpu6050

--- main.c
+++ main.c
@@ -7,36 +7,59 @@
 #include<sysclock/sysclock.h>
 #include<uart/uart.h>
 #include<i2c/i2c.h>
+
+#include<gy86/gy86.h>
 #include<bldc/quad_bldc.h>
 #include<rc_receiver/rc_receiver.h>
 
 void main(void)
 {
+	// setup clock to run at 72 MHz
 	change_sys_clock_source(HSE, 8000000);
 	setup_pll_module(HSE, 72000000);
 	change_sys_clock_source(PLL, 72000000);
 
+	// to enable port c GPIO
 	RCC->RCC_APB2ENR |= (1<<4);
 
+	// GPIO Port C pin 13 setup as output, it is the blink led on the bluepill board
 	GPIOC->GPIO_CRH &= 0xFF0FFFFF;
 	GPIOC->GPIO_CRH |= 0x00200000;
 	GPIOC->GPIO_ODR |= (1 << 13);
 
+	// init rtc clock
 	rtc_init();
-	//uart_init(9600);
+	i2c_init();
+	uart_init(9600);
 
-	uint64_t until_micros = get_now_micros();
+	mpu_init();
+
+	uint8_t dev_addr = 0;
+	char c_dev_addr[3]; c_dev_addr[2] = '\n';
+	while(dev_addr < 0x7f)
+	{
+		delay_for_ms(10);
+		if(i2c_detect(dev_addr))
+		{
+			stringify_8(c_dev_addr, dev_addr);
+			uart_write_blocking(c_dev_addr, 3);
+		}
+		dev_addr++;
+	}
 
 	while(1)
 	{
 		GPIOC->GPIO_ODR ^= (1 << 13);
 
-		/*uint32_t rtc_count = get_now();
-		char c_rtc_count[11];
-		c_rtc_count[10] = '\n';
-		stringify_32(c_rtc_count, rtc_count);
-		uart_write_blocking(c_rtc_count, 11);*/
+		MPUdatascaled mpuData;
+		get_scaled_MPUdata(&mpuData);
 
-		delay_for_ms(323);
+		uint32_t x_acc = mpuData.accl.xi;
+		char c_x_acc[11];
+		c_x_acc[10] = '\n';
+		stringify_32(c_x_acc, x_acc);
+		uart_write_blocking(c_x_acc, 11);
+
+		delay_for_ms(1000);
 	}
 }

87c494016af84e4365f1a1c07872b4a4e5f131d9
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-17 21:40:31 +0530

    until time added to make certain logic easier to control and time

--- delay/delay.c
+++ delay/delay.c
@@ -13,8 +13,18 @@ void delay_for_ms(uint64_t millis)
 	delay_for_us(millis * 1000);
 }
 
+void delay_until_ms(uint64_t until_millis)
+{
+	delay_until_us(until_millis * 1000);
+}
+
 void delay_for_us(uint64_t micros)
 {
 	uint64_t now = get_now_micros();
 	while(get_now_micros() - now < micros);
+}
+
+void delay_until_us(uint64_t until_micros)
+{
+	while(get_now_micros() < until_micros);
 }
--- delay/delay.h
+++ delay/delay.h
@@ -7,6 +7,10 @@ void delay_for(volatile unsigned int clocks);
 
 void delay_for_ms(uint64_t millis);
 
+void delay_until_ms(uint64_t until_millis);
+
 void delay_for_us(uint64_t micros);
+
+void delay_until_us(uint64_t until_micros);
 
 #endif

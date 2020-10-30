b4cf8e3528037fe12404ee7c61b6e09e13674fd7
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-10-30 22:33:38 +0530

    --

--- main.c
+++ main.c
@@ -218,6 +218,8 @@ void main(void)
 				//end_ps = stringify_float(end_ps, aux1); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
 				//end_ps = stringify_float(end_ps, aux2); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
 
+				end_ps = stringify_integer(end_ps, chan_ret[0]); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
+				end_ps = stringify_integer(end_ps, chan_ret[1]); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
 				end_ps = stringify_integer(end_ps, chan_ret[2]); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
 				end_ps = stringify_integer(end_ps, chan_ret[3]); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
 				end_ps = stringify_integer(end_ps, chan_ret[4]); *end_ps = ' '; end_ps++; *end_ps = '\t'; end_ps++;
